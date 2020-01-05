8a6a990c93aa518ac649f25560d99dbab75ddd88
Vladimir_Shmik <indigosunset00@gmail.com>
2020-01-05 13:06:36 +0600

    Верстка

---
+++ js/verticalslider.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.slick-vertical').slick({
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 3,
+        autoplay: true,
+        dots: true,
+    });
+});
